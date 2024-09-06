fd5f868d2d128563c6ab8aa34812a0521b100dfc
Vik <victorch9@gmail.com>
2024-09-06 17:07:26 +0300

    name in setup

--- integrator/main.py
+++ integrator/main.py
@@ -61,22 +61,30 @@ def install_package(package_name):
 
 def main():
     parser = argparse.ArgumentParser(description="Description of your CLI tool")
-    parser.add_argument('install', help="Library to install and integrate")
+    subparsers = parser.add_subparsers(dest='command', help="command to run")
+
+    install_parser = subparsers.add_parser('install', help="Install Python packages using pip.")
+    install_parser.add_argument('library_name', help="Library to install and integrate")
     parser.add_argument('-p', '--prompt', help="What feature of the library do you want to integrate")
     parser.add_argument('-f', '--files', help="Files where you want these changes to be integrated at")
 
     args = parser.parse_args()
-    print(f'{args.install} {args.prompt} {args.files}')
-    install_package(args.install)
-    current_path = os.getcwd()
-    print(f"Project structure for: {current_path}")
-    structure = get_python_project_structure(start_path=current_path, skip_dirs=['.venv', 'node_modules, public, dist, dev_env'])
-    print(structure)
-    content = get_file_contents('requirements.txt')
-    print(f"Contents of {'requirements.txt'}:")
-    print(content)
-    # pass package json, pass content of file, structured prompt
-    prompt = f'I have a python project with and I want to integrate {args.install} into it. {args.prompt}. This is my project structure and requirements.txt : \n {structure} \n dependecies : {content}'
+    print(f'{args.prompt} {args.files}')
+    if args.command == 'install':
+            install_package(args.library_name)
+            print("Library installed")
+    else:
+        parser.print_help()
+    # current_path = os.getcwd()
+    # print(f"Project structure for: {current_path}")
+    # structure = get_python_project_structure(start_path=current_path, skip_dirs=['.venv', 'node_modules, public, dist, dev_env'])
+    # print(structure)
+    # content = get_file_contents(args.files)
+    # dependencies = 
+    # print(f"Contents of {'requirements.txt'}:")
+    # print(content)
+    # # pass package json, pass content of file, structured prompt
+    prompt = f'Generate code for my python project to integrate {args.library_name} library. Add a REST endpoint to process payment requests using stripe. Respond with code only.'
     analysis = analyze_with_openai(prompt)
     print(analysis)
     # update local files
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'mytool=integrator.main:main',
+            'integrator=integrator.main:main',
         ],
     },
 )
